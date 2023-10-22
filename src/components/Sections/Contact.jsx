0b12afe5bc1caf57585a7a93a75095f669068681
soshal dahal <soshal.dahal@kotuko.it>
2023-10-22 22:43:31 +0545

    fix

--- src/components/Sections/Contact.jsx
+++ src/components/Sections/Contact.jsx
@@ -94,14 +94,14 @@ export default function Contact() {
 
             <ImageGrid className="col-xs-12 col-sm-12 col-md-6 col-lg-6">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14127.82584871158!2d85.36529494999999!3d27.7186305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb197956777723%3A0x2fcd3c9fb9a4de2b!2sKotuko%20Nepal!5e0!3m2!1sen!2snp!4v1677821095658!5m2!1sen!2snp"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3568.019994199398!2d87.27633527604401!3d26.583733674149787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ef6df1a8127841%3A0x6f7867da5c795a8d!2sSunsari%20Food%20and%20Beverage%20Udhyog!5e0!3m2!1sen!2snp!4v1697992771866!5m2!1sen!2snp"
                 width="600"
                 height="450"
                 style={{ border: 0 }}
                 allowfullscreen=""
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"
-                title="test"
+                title="Map"
               ></iframe>
             </ImageGrid>
           </div>
